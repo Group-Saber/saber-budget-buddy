65ea95fc888c78c6233e967977574f7467074f05
aarteaga53 <andrewarteaga123@gmail.com>
2022-10-01 13:15:30 -0700

    Assignment 3.3

--- budget_buddy_backend/budget_buddy_app/views.py
+++ budget_buddy_backend/budget_buddy_app/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def say_hello(request):
     return HttpResponse('Hello World')
+
+def andrew_info(request):
+    return HttpResponse('Andrew Arteaga - Back End')
