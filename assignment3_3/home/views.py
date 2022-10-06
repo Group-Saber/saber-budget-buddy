967410e33844b63885a803794fc4cbd6e4b2bf8f
ryandemango <notrmango@gmail.com>
2022-10-05 20:49:27 -0700

    Assignment 4.1: adding django-allauth package

--- assignment3_3/home/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Assignment 3 Number 3</br></br>Ryan Austin Mangilin - Backend")
