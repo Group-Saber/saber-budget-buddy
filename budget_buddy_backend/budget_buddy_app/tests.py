c706ff65fa3ede219f5ae19fe3c9ee76bb0d3cd6
aarteaga53 <81718874+aarteaga53@users.noreply.github.com>
2022-10-11 12:34:07 -0700

    Assignment 6 Unit Testing

--- budget_buddy_backend/budget_buddy_app/tests.py
+++ budget_buddy_backend/budget_buddy_app/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Budget
 
 # Create your tests here.
+class BudgetTestCase(TestCase):
+    def setup(self):
+        budget = Budget.objects.create(amount=53)
+
+    def test_budget_created(self):
+        budget = Budget(amount=53)
+        self.assertEqual(budget.amount, 53)
