54b6ccf939f18f3f857b103c0c22aea28b795377
aarteaga53 <andrewarteaga123@gmail.com>
2022-10-15 16:17:04 -0700

    Changes to frontend, combined list and input

--- budget_buddy_frontend/src/App.js
+++ budget_buddy_frontend/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import BudgetsList from './components/BudgetsList'
-import InputBudget from './components/InputBudget'
-import NavBar from './components/NavBar'
+import SidePanel from './components/SidePanel';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -9,11 +8,11 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1>Budget Buddy</h1>
-          <NavBar />
+          <SidePanel />
+          {/* <h1>Budget Buddy</h1>
+          <NavBar /> */}
           <Routes>
             <Route path='/budgets' element={<BudgetsList />}></Route>
-            <Route path='/input' element={<InputBudget />}></Route>
           </Routes>
         </header>
       </div>
