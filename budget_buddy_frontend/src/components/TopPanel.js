90516629d9c67c73c07ab66fcd9b680ae8640c83
aarteaga53 <andrewarteaga123@gmail.com>
2022-10-16 15:02:43 -0700

    Added debt input and display

--- budget_buddy_frontend/src/components/TopPanel.js
+++ budget_buddy_frontend/src/components/TopPanel.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 
 const TopPanel = ({uid}) => {
-    let [name, setName] = useState('')
+    let [name, setName] = useState('null')
 
     useEffect(() => {
         getName()
-    })
+    }, [])
 
     let getName = async () => {
-        let response = await fetch('http://127.0.0.1:8000/app/name/' + uid)
+        let response = await fetch(`http://127.0.0.1:8000/app/name/${uid}`)
         let data = await response.json()
         setName(`${data.first} ${data.last}`)
     }
