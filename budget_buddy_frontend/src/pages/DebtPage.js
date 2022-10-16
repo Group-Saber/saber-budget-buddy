c095363126864f1e5a61e9abf5c1d74d6bcd2ad6
aarteaga53 <andrewarteaga123@gmail.com>
2022-10-16 11:18:11 -0700

    Dashboard update

---
+++ budget_buddy_frontend/src/pages/DebtPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DebtPage = () => {
+    return (
+        <div className="tab-body">
+            <h2>Debt</h2>
+        </div>
+    )
+}
+
+export default DebtPage
