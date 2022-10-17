7e414444e0ffe735f50755a6e746bd4bfaf8a249
ryandemango <notrmango@gmail.com>
2022-10-16 22:17:52 -0700

    A6: Added test cases, test_income_positive and test_debts_negative

--- budget_buddy_backend/budget_buddy_app/migrations/0001_initial.py
+++ budget_buddy_backend/budget_buddy_app/migrations/0001_initial.py
@@ -17,6 +17,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('amount', models.FloatField()),
                 ('inc_amount', models.FloatField()),
+                ('debts_amount', models.FloatField()),
                 ('updated', models.DateTimeField(auto_now=True)),
                 ('created', models.DateTimeField(auto_now_add=True)),
             ],
--- budget_buddy_backend/budget_buddy_app/models.py
+++ budget_buddy_backend/budget_buddy_app/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from .validators import validate_positive
 from .validators import validate_negative
 
-
 # Create your models here.
 
 class Budget(models.Model):
