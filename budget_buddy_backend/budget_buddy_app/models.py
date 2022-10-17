231c2d40d956398dfb0a0b307da073f206f80b7d
ryandemango <notrmango@gmail.com>
2022-10-16 22:17:52 -0700

    A6: Added test cases, test_income_positive and test_debts_negative

--- budget_buddy_backend/budget_buddy_app/models.py
+++ budget_buddy_backend/budget_buddy_app/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from .validators import validate_positive
+from .validators import validate_negative
 
 # Create your models here.
 
 class Budget(models.Model):
     amount = models.FloatField(validators=[validate_positive])
+    inc_amount = models.FloatField(validators=[validate_positive])
+    debts_amount = models.FloatField(validators=[validate_negative])
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(round(self.amount, 2))
+
+    def __str__(self):
+        return str(round(self.inc_amount, 2))
+
+    def __str__(self):
+        return str(round(self.debts_amount, 2))
--- budget_buddy_backend/budget_buddy_app/tests.py
+++ budget_buddy_backend/budget_buddy_app/tests.py
@@ -5,7 +5,21 @@ from .models import Budget
 class BudgetTestCase(TestCase):
     def setup(self):
         budget = Budget.objects.create(amount=53)
+        income = Budget.objects.create(inc_amount=20)
+        debts = Budget.objects.create(debts_amount=-100.00)
 
     def test_budget_created(self):
         budget = Budget(amount=53)
         self.assertEqual(budget.amount, 53)
+
+    def test_budget_created(self):
+        budget = Budget(amount=53)
+        self.assertEqual(budget.amount, 53)
+
+    def test_income_positive(self):
+        income = Budget(inc_amount=20)
+        self.assertGreaterEqual(income.inc_amount, 0) #test if income >= 0
+
+    def test_debts_negative(self):
+        debts = Budget(debts_amount=-100.00)
+        self.assertLessEqual(debts.debts_amount, 0) #test if debt <= 0, because we want debts to be negative
