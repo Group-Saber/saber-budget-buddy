17cd7d372249f29cd7a188a538d748f0d5820273
BrandonDo211 <74513394+BrandonDo211@users.noreply.github.com>
2022-10-18 19:49:51 -0700

    Update tests.py

--- budget_buddy_backend/budget_buddy_app/tests.py
+++ budget_buddy_backend/budget_buddy_app/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from .models import Budget
 # Create your tests here.
+
+class BudgetTestCase(TestCase):
+    def setup(self):
+        budget = Budget.objects.create(amount=53)
+        
+        
+    def test_budget_created(self):
+         budget = Budget(amount=50)
+         self.assertEqual(budget.amount, 50)
+          
+    def test_income_positive(self):
+         income = Budget(inc_amount=20)
+         self.assertGreaterEqual(income.inc_amount, 0) #test if income is less than 0
